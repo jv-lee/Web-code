aacb797fbe7a23cbddb8b750625fa02287dc0047
lijiawei@hclw.online <lijiawei@hclw.online>
2019-04-26 15:48:39 +0800

    ok work

--- web-application/js/index.js
+++ web-application/js/index.js
@@ -55,6 +55,7 @@ onReady(function() {
         }
     };
 
+    //初始化动画
     for (var k in screenAnimateElements) {
         setScrrenAnimateInit(k);
     }
@@ -64,49 +65,61 @@ onReady(function() {
         var top = document.documentElement.scrollTop;
         console.log(top);
 
+        //设置右侧菜单动画初始化
+        if (top > 80) {
+            EleUtil.addCls(EleUtil.getEle('.menu'),'menu-animate-init');
+        } else {
+            EleUtil.delCls(EleUtil.getEle('.menu'),'menu-animate-init');
+        }
+
         if (top > 1) {
             playScrrenAnimateDone('.screen-1');
             addNavActive(0);
         }
 
-        if (top > 80) {
-        	EleUtil.addCls(EleUtil.getEle('.menu'),'menu-animate-init');
-        } else {
-            EleUtil.delCls(EleUtil.getEle('.menu'),'menu-animate-init');
+        if (top > 1 || top < 540) {
+            addTipActive(0);
         }
 
         if (top > (640 - 100) * 1) {
             playScrrenAnimateDone('.screen-2');
             addNavActive(1);
+            addTipActive(1);
         }
 
         if (top > (640 - 100) * 2) {
             playScrrenAnimateDone('.screen-3');
             addNavActive(2);
+            addTipActive(2);
         }
 
         if (top > (640 - 100) * 3) {
             playScrrenAnimateDone('.screen-4');
             addNavActive(3);
+            addTipActive(3);
         }
 
         if (top > (640 - 100) * 4) {
             playScrrenAnimateDone('.screen-5');
             addNavActive(4);
+            addTipActive(4);
         }
     };
 
     // 双向导航绑定设置
+    var typeItems = document.querySelectorAll('.screen-3-content-types-item');
     var navItems = document.querySelectorAll('.header-nav-item');
     var menuItems = document.querySelectorAll('.menu-item');
+    var tip = document.querySelector(".header-nav-tip");
+    var currentIndex = 0;
+
+    //设置tip选中样式
+    var addTipActive = function(index){
+        tip.style.left = (index * 104) + "px";
+    };
 
     //设置点击选中样式
     var addNavActive = function(index) {
-        for (var i = 0; i < navItems.length; i++) {
-            EleUtil.delCls(navItems[i], 'header-nav-item-active');
-        }
-        EleUtil.addCls(navItems[index], 'header-nav-item-active');
-
         for (var i = 0; i < menuItems.length; i++) {
             EleUtil.delCls(menuItems[i], 'menu-item-active');
         }
@@ -114,30 +127,39 @@ onReady(function() {
     };
     addNavActive(0);
 
-    //设置导航栏点击 跳转及修改样式
-    var setNavJump = function(i, items) {
+
+    //导航栏item设置点击事件监听方法
+    var setNavClickListener = function(i, items) {
         var item = items[i];
         item.onclick = function() {
+            currentIndex = i;
             addNavActive(i);
             document.documentElement.scrollTop = (i * 640);
         };
     };
+
+
+    //导航栏item鼠标移动设置监听方法
+    var setNavMouseoverListener = function(index,items){
+    	items[index].onmouseover = function(){
+            addTipActive(index);
+    	};
+
+    	items[index].onmouseout = function(){
+            addTipActive(currentIndex);
+    	};
+    };
+
+    // 循环设置所有导航item 点击设置页面scrollTop属性
     for (var i = 0; i < navItems.length; i++) {
-        setNavJump(i, navItems);
-        setNavJump(i, menuItems);
+        setNavClickListener(i,navItems);
+        setNavClickListener(i, menuItems);
     }
 
-
-    // var setNavActive = function(index,items){
-    // 	items[index].onmouseover = function(){
-    // 		items[index].style.left = (index*104)+ "px";
-    // 	};
-
-    // 	var activeIndex = 0;
-    // 	items[index].onmouseout = function(){
-
-    // 	};
-    // };
+    //设置鼠标滑入滑出 下标移动
+    for (var i = 0; i < navItems.length; i++) {
+        setNavMouseoverListener(i,navItems);
+    }
 
     setTimeout(function() {
         playScrrenAnimateDone('.screen-1');
