eda1f322c8f05f2368c7569bfa24dd13ea0cac54
jv.lee@foxmail.com <jv.lee@foxmail.com>
2019-10-11 23:53:20 +0800

    update code

--- 幕淘/js/index.js
+++ 幕淘/js/index.js
@@ -1,42 +1,42 @@
-(function($){
-	'use strict'
+(function($) {
+    'use strict'
 
-// 调用封装的下拉菜单组件
-    var json = [
-{
-    "url":"###",
-    "name":"已买到的宝贝"
-},
-{
-    "url":"###",
-    "name":"我的足迹"
-},
-{
-    "url":"###",
-    "name":"我的优惠券"
-}
-];
+    // 调用封装的下拉菜单组件
+    var json = [{
+            "url": "###",
+            "name": "已买到的宝贝"
+        },
+        {
+            "url": "###",
+            "name": "我的足迹"
+        },
+        {
+            "url": "###",
+            "name": "我的优惠券"
+        }
+    ];
+
+    //menu drapdown	
     $('.dropdown').on('dropdown-start dropdown-resume dropdown-pause dropdown-stop', function(e) {
         console.log(e.type);
         var $this = $(this);
 
         if (e.type == 'dropdown-start') {
-            
-            // if(!$this.data('loaded')){
-            //     var $layer = $this.find('.dropdown-layer'),
-            //     html = '';
 
-            //     setTimeout(function(){
-            //         for (var i = 0; i < json.length; i++) {
-            //             html += '<li><a href="'+json[i].url +'" target="_blank" class="menu-item">'+json[i].name+'</a></li>'
-            //         }
-            //         $layer.html(html);
-            //         $this.data('loaded',true);
-            //     },1000);
-            // }
+            if (!$this.data('loaded')) {
+                var $layer = $this.find('.dropdown-layer'),
+                    html = '';
+
+                setTimeout(function() {
+                    for (var i = 0; i < json.length; i++) {
+                        html += '<li><a href="' + json[i].url + '" target="_blank" class="menu-item">' + json[i].name + '</a></li>'
+                    }
+                    $layer.html(html);
+                    $this.data('loaded', true);
+                }, 1000);
+            }
         }
     });
-
     $('.dropdown').dropdown({
         event: 'hover',
         css3: true,
@@ -46,6 +46,57 @@
         active: 'menu'
     });
 
+    //header search
+    var $headerSearch = $('#header-search');
+    var html = '',
+        maxNum = 10;
+    $headerSearch.on('search-getData', function(e, data, $layer) {
+            var $this = $(this),
+            html = createHeaderSearchLayer(data, maxNum);
+            $layer.html(html);
+
+            if (html) {
+                $this.search('showLayer');
+            } else {
+                $this.search('hideLayer');
+            }
+        }).on('search-noData', function(e, $layer) {
+            $(this).search('hideLayer');
+            $layer.html('');
+        })
+        //代理事件通过事件冒泡拦截的方式 来控制生效的点击
+        .on('click', '.search-layer-item', function() {
+            //获取当前的搜索提示内容item的内容 填充到input
+            $headerSearch.search('setinputVal',$(this).html());
+            $headerSearch.search('submit');
+        });
+    $headerSearch.search({
+        autocomplete: true,
+        css3: false,
+        js: false,
+        animation: 'fade'
+    });
+
+    function removeHtmlTags(str) {
+        return str.replace(/<(?:[^>'"]|"[^"]*"|'[^']*')*>/g, "");
+    }
+
+    //创建html结构
+    function createHeaderSearchLayer(data, maxNum) {
+        var html = '',
+            dataNum = data['result'].length;
+
+        //没有搜索
+        if (dataNum === 0) {
+            return '';
+        }
+
+        for (var i = 0; i < dataNum; i++) {
+            if (i >= maxNum) break;
+
+            html += '<li class="search-layer-item text-ellipsis">' + data['result'][i][0] + '</li>';
+        }
+        return html;
+    }
 
 }(jQuery))
-
--- 幕淘/js/search.js
+++ 幕淘/js/search.js
@@ -1,75 +1,103 @@
-(function($){
-	'use strict'
+(function($) {
+    'use strict';
 
-	var $search = $('.search'),
-		$form = $search.find('.search-form'),
-		$input = $search.find('.search-inputbox'),
-		$btn = $search.find('.search-btn'),
-		$layer = $search.find('.search-layer');
+    function Search(elem, options) {
+        this.$elem = $(elem);
+        this.options = options;
 
-	//验证
-	$form.on('submit',function(){
-		if($.trim($input.val()) === ''){
-			return false;
-		}
-	});
+        this.$form = this.$elem.find('.search-form');
+        this.$input = this.$elem.find('.search-inputbox');
+        this.$layer = this.$elem.find('.search-layer');
 
-	//自动完成
-	$input.on('input',function(){
-		var url = "https://suggest.taobao.com/sug?code=utf-8&_ksTS=1570786324192_819&callback=jsonp820&k=1&area=c2c&bucketid=10&q="
-		+ encodeURIComponent($.trim($input.val()));
+        //父类代理绑定事件 - 代理替换方法内部this指向
+        this.$elem.on('click', '.search-btn', $.proxy(this.submit, this));
+        if (this.options.autocomplete) {
+            this.autocomplete();
+        }
+    }
+    //默认参数
+    Search.DEFAULTS = {
+        autocomplete: false, //是否自动完成
+        url: 'https://suggest.taobao.com/sug?code=utf-8&_ksTS=1570786324192_819&callback=jsonp820&k=1&area=c2c&bucketid=10&q=', //搜索地址
+        css3: false,
+        js: false,
+        animation: 'fade'
+    };
+    //提交功能
+    Search.prototype.submit = function() {
+        if (this.getInputVal() === '') {
+            return false;
+        }
+        this.$form.submit();
+    };
+    //自动提示
+    Search.prototype.autocomplete = function() {
+        //处理事件冲突，input获取焦点时显示提示内容， 因为失去焦点事件与click冲突， 所以直接使用document绑定最上层点击事件来隐藏，通过input拦截点击冒泡来取消事件传递
+        this.$input
+            .on('input', $.proxy(this.getData, this))
+            .on('focus', $.proxy(this.showLayer, this))
+            .on('click', function() {
+                return false;
+            });
+        $(document).on('click',$.proxy(this.hideLayer,this));
+        //引入显示隐藏模块
+        this.$layer.showHide(this.options);
+    };
+    //获取数据
+    Search.prototype.getData = function() {
+        var self = this;
+        $.ajax({
+            url: this.options.url + this.getInputVal(),
+            timeout: 5000,
+            dataType: 'jsonp'
+        }).done(function(data) {
+        	//事件通知  参数传递使用数组的方式
+            self.$elem.trigger('search-getData', [data,self.$layer]);
+        }).fail(function() {
+            self.$elem.trigger('search-noData', [self.$layer]);
+        });
+    };
+    //显示下拉层
+    Search.prototype.showLayer = function() {
+        //没有内容不显示
+        if (this.$layer.children().length === 0) return;
+        this.$layer.showHide('show');
+    };
+    //隐藏下拉层
+    Search.prototype.hideLayer = function() {
+        this.$layer.showHide('hide');
+    };
+    //获取输入数据
+    Search.prototype.getInputVal = function() {
+        return $.trim(this.$input.val());
+    };
+    //设置输入数据
+    Search.prototype.setInputVal = function(val){
+    	this.$input.val(removeHtmlTags(val));
+    };
 
-		$.ajax({
-			url:url,
-			timeout:5000,
-			dataType:'jsonp'
-		}).done(function(data){
-			console.log(data);
-			var html = '',
-				dataNum = data['result'].length,
-				maxNum = 10;
-
-				//没有搜索
-				if (dataNum === 0) {
-					$layer.hide().html('');
-					return;
-				}
-
-				for (var i = 0; i < dataNum; i++) {
-					if (i>=maxNum)break;
-
-					html += '<li class="search-layer-item text-ellipsis">'+data['result'][i][0]+'</li>';
-				}
-				$layer.html(html).show();
-		}).fail(function (){
-			console.log('request fail. . .')
-			$layer.hide().html('');
-		}).always(function(){
-			console.log('why always me!');
-		});
-	});
-
-	//处理事件冲突，input获取焦点时显示提示内容， 因为失去焦点事件与click冲突， 所以直接使用document绑定最上层点击事件来隐藏，通过input拦截点击冒泡来取消事件传递
-	$input.on('focus',function(){
-		$layer.show();
-	}).on('click',function(){
-		return false;
-	});
-	$(document).on('click',function(){
-		$layer.hide();
-		return 
-	});
-
-	//代理事件通过事件冒泡拦截的方式 来控制生效的点击
-	$layer.on('click','.search-layer-item',function(){
-		//获取当前的搜索提示内容item的内容 填充到input
-		$input.val(removeHtmlTags($(this).html()));
-		$form.submit();
-		$input.parents('form').submit();
-	})
-
+    //删除字符串中html标签
 	function removeHtmlTags(str){
 		return str.replace(/<(?:[^>'"]|"[^"]*"|'[^']*')*>/g,"");
 	}
+
+	//添加搜索插件
+    $.fn.extend({
+        search: function(option,value) {
+            return this.each(function() {
+                var $this = $(this),
+                    search = $this.data('search'),
+                    options = $.extend({}, Search.DEFAULTS, $this.data(), typeof option === 'object' && option);
+
+                if (!search) {
+                    $this.data('search', search = new Search($this, options));
+                }
+
+                if (typeof search[option] === 'function') {
+                    search[option](value);
+                }
+            });
+        }
+    });
 
 }(jQuery));
