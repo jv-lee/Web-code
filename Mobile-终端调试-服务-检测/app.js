045796a78ce80067ead76792d8c50ed0026d2f5a
lijiawei@hclw.online <lijiawei@hclw.online>
2019-11-25 19:23:56 +0800

    update code

--- Mobile-终端调试-服务-检测/app.js
+++ Mobile-终端调试-服务-检测/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var serveStatic = require('serve-static');
 var app = express();
 
-app.use(serveStatic('E:/works/Web-code/Mobile-终端调试-服务-检测/'));
+// app.use(serveStatic('E:/works/Web-code/Mobile-终端调试-服务-检测/'));
+app.use(serveStatic('./'));
 // app.use(serveStatic('/Users/jv.lee/workspaces/web/web-code/Mobile-屏幕适配/'));
 app.listen(8080,function(){
 	console.log('App listening at prot 8080');
